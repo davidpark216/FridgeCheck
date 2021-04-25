9111bf292a757a82fd28cdf9f4186ae3cad48eda
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-04-25 13:33:27 +0900

    add login function

--- fridecheck/src/components/Login.tsx
+++ fridecheck/src/components/Login.tsx
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-function Login() {
-  return <div>여기는 로그인 구간입니다</div>;
+interface props {
+  loginPage: boolean;
+}
+
+function Login({ loginPage }: props) {
+  return loginPage ? (
+    <LoginWrap>
+      <div>여기는 로그인 구간입니다</div>
+      <div>x</div>
+    </LoginWrap>
+  ) : (
+    <div></div>
+  );
 }
 
 export default Login;
+
+const LoginWrap = styled.div``;
--- fridecheck/src/components/Nav.tsx
+++ fridecheck/src/components/Nav.tsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { Login, Signup } from "../components";
 
+interface props {
+  handleLoginClick: () => void;
+}
 //메뉴들이 모인 공간
-function Nav() {
-  return <div>여기는 Nav공간입니다.</div>;
+function Nav({ handleLoginClick }: props) {
+  return (
+    <NavWrap>
+      <button onClick={handleLoginClick}>로그인</button>
+      <button>회원가입</button>
+      <button>유통기한 팁</button>
+    </NavWrap>
+  );
 }
 
 export default Nav;
+
+const NavWrap = styled.div``;
