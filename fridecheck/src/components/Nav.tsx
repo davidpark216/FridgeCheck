f3fc73ff49b2a07cd9b5aa29b8da2be451efe325
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-05-05 13:33:56 +0900

    Add cold and frozen button function

--- fridecheck/src/components/Nav.tsx
+++ fridecheck/src/components/Nav.tsx
@@ -4,16 +4,18 @@ import { Login, Signup } from "../components";
 
 interface props {
   handleLoginClick: () => void;
+  handleCold: () => void;
+  handleFrozen: () => void;
 }
 //메뉴들이 모인 공간
-function Nav({ handleLoginClick }: props) {
+function Nav({ handleLoginClick, handleCold, handleFrozen }: props) {
   return (
     <NavWrap>
       <Button onClick={handleLoginClick}>로그인</Button>
       <Button>회원가입</Button>
       <Button>유통기한 팁</Button>
-      <Button>냉장실 추가</Button>
-      <Button>냉동실 추가</Button>
+      <Button onClick={handleCold}>냉장실 추가</Button>
+      <Button onClick={handleFrozen}>냉동실 추가</Button>
     </NavWrap>
   );
 }
