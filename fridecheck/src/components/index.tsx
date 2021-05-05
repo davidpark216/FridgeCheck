6e4ba83a89a8b10bc0eb8c0ddd4253acf02bbddb
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-05-05 13:34:24 +0900

    Add AddArea file

--- fridecheck/src/components/index.tsx
+++ fridecheck/src/components/index.tsx
@@ -3,5 +3,6 @@ import FrozenStorage from "./FrozenStorage";
 import Login from "./Login";
 import Signup from "./Signup";
 import Nav from "./Nav";
+import AddArea from "./AddArea";
 
-export { ColdStorage, FrozenStorage, Login, Signup, Nav };
+export { ColdStorage, FrozenStorage, Login, Signup, Nav, AddArea };
