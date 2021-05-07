704953889edae839afb87a6212cfd7f56d1df50c
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-05-07 23:15:12 +0900

    delete 유통기한

--- fridecheck/src/components/Nav.tsx
+++ fridecheck/src/components/Nav.tsx
@@ -4,18 +4,17 @@ import { Login, Signup } from "../components";
 
 interface props {
   handleLoginClick: () => void;
-  handleCold: () => void;
-  handleFrozen: () => void;
+  handleOpenCold: () => void;
+  handleOpenFrozen: () => void;
 }
 //메뉴들이 모인 공간
-function Nav({ handleLoginClick, handleCold, handleFrozen }: props) {
+function Nav({ handleLoginClick, handleOpenCold, handleOpenFrozen }: props) {
   return (
     <NavWrap>
       <Button onClick={handleLoginClick}>로그인</Button>
       <Button>회원가입</Button>
-      <Button>유통기한 팁</Button>
-      <Button onClick={handleCold}>냉장실 추가</Button>
-      <Button onClick={handleFrozen}>냉동실 추가</Button>
+      <Button onClick={handleOpenCold}>냉장실 추가</Button>
+      <Button onClick={handleOpenFrozen}>냉동실 추가</Button>
     </NavWrap>
   );
 }
