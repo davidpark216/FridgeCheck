4cb2bab7b02a53a82d1a170c19c7ebd6826e2a0e
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-05-07 23:14:04 +0900

    change structure and add type function, ready to add function

--- fridecheck/src/components/AddArea.tsx
+++ fridecheck/src/components/AddArea.tsx
@@ -1,30 +1,85 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+interface items {
+  item: string;
+  quantity: string;
+}
+
 interface props {
   addCold: boolean;
   addFrozen: boolean;
+  coldItems: items[];
+  frozenItmes: items[];
   setAddCold: any;
   setAddFrozen: any;
+  setColdItems: any;
+  setFrozenItems: any;
 }
-function AddArea({ addCold, addFrozen, setAddCold, setAddFrozen }: props) {
+function AddArea({
+  addCold,
+  addFrozen,
+  coldItems,
+  frozenItmes,
+  setAddCold,
+  setAddFrozen,
+  setColdItems,
+  setFrozenItems,
+}: props) {
+  const [addColdItem, setAddColdItem] = useState({ item: "", quantity: "" });
+
+  const [addFrozenItem, setAddFrozenItem] = useState({
+    item: "",
+    quantity: "",
+  });
+
+  const handleTypeCold = (
+    key: string,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { value } = e.target;
+    setAddColdItem({ ...addColdItem, [key]: value });
+  };
+
+  const handleTypeFrozen = (
+    key: string,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { value } = e.target;
+    setAddFrozenItem({ ...addFrozenItem, [key]: value });
+  };
+
+  const handleAddCold = () => {
+    setColdItems({});
+  };
+
   return (
     <AddAreaWrap>
       {addCold ? (
         <InputArea>
           <div className="upperArea">
             <span>냉장칸 추가 물품</span>
-            <span
+            <button
               className="close"
               onClick={() => {
                 setAddCold(false);
               }}
             >
               x
-            </span>
+            </button>
           </div>
-
-          <input type="text" />
+          <div className="coldItem">
+            <span className="text">물품 : </span>
+            <input type="text" onChange={(e) => handleTypeCold("item", e)} />
+          </div>
+          <div className="coldQuantity">
+            <span className="text">양 : </span>
+            <input
+              type="text"
+              onChange={(e) => handleTypeCold("quantity", e)}
+            />
+          </div>
+          <button>추가</button>
         </InputArea>
       ) : (
         <div />
@@ -33,17 +88,25 @@ function AddArea({ addCold, addFrozen, setAddCold, setAddFrozen }: props) {
         <InputArea>
           <div className="upperArea">
             <span>냉동칸 추가 물품</span>
-            <span
+            <button
               className="close"
               onClick={() => {
                 setAddFrozen(false);
               }}
             >
               x
-            </span>
+            </button>
           </div>
 
-          <input type="text" />
+          <div className="coldItem">
+            <span className="text">물품 : </span>
+            <input type="text" onChange={(e) => handleTypeFrozen("item", e)} />
+          </div>
+          <div className="coldQuantity">
+            <span className="text">양 : </span>
+            <input type="text" onChange={(e) => handleTypeFrozen("item", e)} />
+          </div>
+          <button>추가</button>
         </InputArea>
       ) : (
         <div />
