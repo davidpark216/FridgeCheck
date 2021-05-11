b35c6a2d85a47a34f1131d7d8ce809ba624b0ea6
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-05-11 23:20:40 +0900

    Add grid and chage element(span to div)

--- fridecheck/src/components/ColdStorage.tsx
+++ fridecheck/src/components/ColdStorage.tsx
@@ -7,20 +7,20 @@ function ColdStorage({ coldItems }: props) {
   return (
     <ColdStorageWrap>
       <ColdArea>
-        <div className="coldHead">
-          <div className="coldHeadName">이름</div>
-          <div className="coldHeadQuantity">양</div>
-          <div className="coldHeadDate">날짜</div>
+        <div id="coldHead">
+          <div id="coldHeadName">이름</div>
+          <div id="coldHeadQuantity">양</div>
+          <div id="coldHeadDate">날짜</div>
         </div>
         {coldItems.map((el: any) => (
           <div>
-            <div className="coldItem">
-              <span className="coldName">{el.item}</span>
-              <span className="coldQU">
+            <div id="coldItem">
+              <div className="coldName">{el.item}</div>
+              <div className="coldQU">
                 <span className="coldQuantity">{el.quantity}</span>
                 <span className="coldUnit">{el.unit}</span>
-              </span>
-              <span className="coldDate">{el.date}</span>
+              </div>
+              <div className="coldDate">{el.date}</div>
             </div>
           </div>
         ))}
@@ -32,14 +32,15 @@ function ColdStorage({ coldItems }: props) {
 export default ColdStorage;
 
 const ColdStorageWrap = styled.div`
-  .coldItem {
+  #coldHead {
+    display: grid;
+    grid-template-columns: 100px 100px 200px;
+    border-bottom: 1px solid;
+  }
+  #coldItem {
     margin-bottom: 10px;
-  }
-  .coldQuantity {
-    margin-left: 10px;
-  }
-  .coldDate {
-    margin-left: 10px;
+    display: grid;
+    grid-template-columns: 100px 100px 200px;
   }
 `;
 
