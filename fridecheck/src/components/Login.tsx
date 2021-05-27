44b597101d9ba98610c891e8775c9ac876abbce3
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-05-27 23:45:31 +0900

    Add css

--- fridecheck/src/components/Login.tsx
+++ fridecheck/src/components/Login.tsx
@@ -11,25 +11,41 @@ function Login({ loginPage, setLoginPage, handleLogin }: props) {
     setLoginPage(false);
   };
 
+  const [loginInfo, setLoginInfo] = useState({ userId: "", password: "" });
+
+  const handleInput = (key: string, e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setLoginInfo({ ...loginInfo, [key]: value });
+  };
+
   return loginPage ? (
     <LoginModal>
       <LoginWrap>
         <CloseLogin>
           <Button onClick={closeLogin}>x</Button>
         </CloseLogin>
-
-        <h3 id="loginHead">로그인</h3>
-        <div id="inputBox">
-          <div id="inputId">
-            <input type="text" placeholder="아이디" />
+        <LoginContent>
+          <h3 id="loginHead">로그인</h3>
+          <div id="inputBox">
+            <div id="inputId">
+              <Input
+                type="text"
+                placeholder="아이디"
+                onChange={(e) => handleInput("userId", e)}
+              />
+            </div>
+            <div id="inputPassword">
+              <Input
+                type="password"
+                placeholder="비밀번호"
+                onChange={(e) => handleInput("password", e)}
+              />
+            </div>
+            <div id="loginButton">
+              <Button onClick={() => handleLogin()}>로그인</Button>
+            </div>
           </div>
-          <div id="inputPassword">
-            <input type="password" placeholder="비밀번호" />
-          </div>
-          <div id="loginButton">
-            <Button onClick={() => handleLogin()}>로그인</Button>
-          </div>
-        </div>
+        </LoginContent>
       </LoginWrap>
     </LoginModal>
   ) : (
@@ -54,6 +70,11 @@ const LoginWrap = styled.div`
   width: 30vw;
   height: 40vh;
   background: white;
+  text-align: center;
+`;
+
+const LoginContent = styled.div`
+  padding-top: 20px;
 `;
 
 const CloseLogin = styled.div`
@@ -62,4 +83,8 @@ const CloseLogin = styled.div`
 
 const Button = styled.button`
   ${({ theme }) => theme.common.defaultButton}
+`;
+
+const Input = styled.input`
+  margin-bottom: 20px;
 `;
